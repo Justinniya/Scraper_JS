5c3758e436216eed4e48df6ded0e975db7b6b420
Justinniya <justindelavega00@gmail.com>
2025-05-21 18:28:17 +0800

    listing airbnb

--- app/login_airbnb.js
+++ app/login_airbnb.js
@@ -2,15 +2,13 @@ const { chromium } = require('playwright');
 const fs = require('fs');
 
 async function loginToAirbnb(email, password) {
-    try{
-        const browser = await chromium.launch({ headless: false, args: ['--start-maximized'] });
-        const context = await browser.newContext({userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'});
+        const browser = await chromium.launch({ headless: true ,args: ['--window-size=1920,1080','--no-sandbox'] });
+        const context = await browser.newContext({userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',viewport: { width: 1420, height: 975 }});
         const page = await context.newPage();
         await page.waitForTimeout(5000);
         await page.goto('https://www.airbnb.com/login');
         await page.waitForTimeout(5000);
     // loginToAirbnb("justindelavega00@gmail.com", "Emjaycee83849724")
-    await page.screenshot({ path : 'f1.png', fullPage: true });
         console.log(email,password);
         try{
             const accept_cookies = await page.locator('xpath=//*[@id="react-application"]/div/div/div[1]/div/div[3]/section/div/div[2]/div[1]/button').isVisible();
@@ -30,25 +28,22 @@ async function loginToAirbnb(email, password) {
             await page.keyboard.press(typing);
             await page.waitForTimeout(1000);
         }
-        await page.screenshot({ path : 'f2.png', fullPage: true });
         await page.keyboard.press('Enter');
         await page.waitForTimeout(15000);
-        await page.screenshot({ path : 'f3.png', fullPage: true });
         await page.waitForTimeout(2000);
         for(const typing of password){
             await page.keyboard.press(typing);
             await page.waitForTimeout(1000);
         }
-        await page.screenshot({ path : 'f4.png', fullPage: true });
         await page.waitForTimeout(2000);
         await page.keyboard.press('Enter');
-        await page.screenshot({ path : 'screenshot.png', fullPage: true });
         await page.waitForTimeout(5000);
         console.log(await page.url());
         await page.waitForTimeout(10000);
-        await page.screenshot({ path : 'result.png', fullPage: true });
-        await page.waitForTimeout(100000);
-        if (await page.url() === 'https://www.airbnb.com/') {
+        await page.waitForTimeout(10000);
+        await page.goto('https://www.airbnb.com/');
+        await page.waitForTimeout(20000);
+        if (await page.url() == 'https://www.airbnb.com/') {
             await page.screenshot({ path : 'correct.png', fullPage: true });
             const cookies = await context.cookies();
             fs.writeFileSync('airbnb.json', JSON.stringify(cookies, null, 2));
@@ -61,10 +56,6 @@ async function loginToAirbnb(email, password) {
             await browser.close();
             return false;
         }
-    }catch(err){
-        console.log('Error:', err);
-        return false;
-    }
 }
 
 module.exports = { loginToAirbnb };
