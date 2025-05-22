8e30fd6f2f82865102147df85ccafcbc1cffe28f
Justinniya <justindelavega00@gmail.com>
2025-05-22 15:27:56 +0800

    login

---
+++ login_airbnb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "[+] Starting Airbnb Login SSH tunnel with autossh..."
+autossh -M 0 -NTC \
+  -o ServerAliveInterval=60 \
+  -o ServerAliveCountMax=3 \
+  -o ExitOnForwardFailure=yes \
+  -o StrictHostKeyChecking=no \
+  -o UserKnownHostsFile=/dev/null \
+  -R 4001:127.0.0.1:4000 \
+  root@dev.d3.net
